cf435760602a81379e9ba305f0e37bfa635f4379
juliahigh <144129297+juliahigh@users.noreply.github.com>
2026-04-07 19:09:06 +0000

    Tue, Apr 7, 2026, 3:09 PM -04:00

--- fpdesign.py
+++ fpdesign.py
@@ -1,2 +1,10 @@
 print ("Welcome to the CS32 cookbook!\nYou tell us your ingredients, and we will recommend a recipe.\nPress Enter to begin.")
-print ("Please list 10 ingredients you have in your kitchen.")
+ingredients = input("Please list 10 ingredients you have in your kitchen, separated by commas.")
+ingredients.split(",")
+
+
+#for each line in ingredients
+#if user input matches ingredients
+#update some variable
+#percentage of ingredients matched = some variable / total ingredients in recipe
+#print percentage of ingredients matched
