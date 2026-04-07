7e3e37d143ad1e5ae37242e9cb24fd184f0955dc
juliahigh <144129297+juliahigh@users.noreply.github.com>
2026-04-07 22:43:43 +0000

    Tue, Apr 7, 2026, 6:43 PM -04:00

--- fpdesign.py
+++ fpdesign.py
@@ -14,15 +14,23 @@ input("Press enter to begin.")
 user_ingredients = []
 user_input = input("Please list up to 10 ingredients you have in your kitchen, separated by commas: ")
 
+#take the user's input and split it along the commas. for each of those ingredients...
 for ingredient in user_input.split(","):
+    #add to the list user_ingredients. and get rid of commas and lowercase everything so you don't have to deal with
+    #confusing between upper and lowercase
     user_ingredients.append(ingredient.strip().lower())
 
 with open('ingredients.csv') as computer_ingredients:
+    #create another empty list, this time for the ingredients in the recipe
     recipe_ingredients = []
+    #for each line of our ingredients file
     for ingredient in computer_ingredients:
+        #add to the actual recipes ingredients
         recipe_ingredients.append(ingredient.strip().lower())
 
+#the total number of ingredients is the number of ingredients in the recipe
 total_ingredients = len(recipe_ingredients)
+
 
 for ingredient in recipe_ingredients:
     if ingredient in user_ingredients:
