d084e860d98578f65521573164a000919481bcee
juliahigh <144129297+juliahigh@users.noreply.github.com>
2026-04-07 19:05:12 +0000

    Tue, Apr 7, 2026, 3:05 PM -04:00

--- fpdesign.py
+++ fpdesign.py
@@ -1,2 +1,2 @@
-print ("Welcome to the CS32 cookbook!")
-
+print ("Welcome to the CS32 cookbook!\nYou tell us your ingredients, and we will recommend a recipe.\nPress Enter to begin.")
+print ("Please list 10 ingredients you have in your kitchen.")
