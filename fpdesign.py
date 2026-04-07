3a25e7fbb4e27dd4904612bb65fd62f8c130122a
juliahigh <144129297+juliahigh@users.noreply.github.com>
2026-04-07 22:15:04 +0000

    Tue, Apr 7, 2026, 6:15 PM -04:00

--- fpdesign.py
+++ fpdesign.py
@@ -1,6 +1,9 @@
+ingredients = open('txts/ingredients.csv')
+matching_ingredients = 0
+total_ingredients = 0
 print ("Welcome to the CS32 cookbook!\nYou tell us your ingredients, and we will recommend a recipe.\nPress Enter to begin.")
 #require an enter to begin
-ingredients = input("Please list 10 ingredients you have in your kitchen, separated by commas.")
+user_ingredients = input("Please list 10 ingredients you have in your kitchen, separated by commas.")
 #Otherwise prompt an error message, "please enter your ingredients separated by commas"
 ingredients.split(",")
 
@@ -10,3 +13,12 @@ ingredients.split(",")
 #update some variable
 #percentage of ingredients matched = some variable / total ingredients in recipe
 #print percentage of ingredients matched
+
+while True:
+    ingredients_line = ingredients.readline()
+    for ingredient in ingredients:
+        if user_ingredients == ingredient:
+            matching_ingredients += 1
+        
+
+#print matching ingredients / total ingredients
