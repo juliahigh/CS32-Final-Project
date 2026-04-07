830ede7341e9d23227a6efe3b92544347cf1108e
juliahigh <144129297+juliahigh@users.noreply.github.com>
2026-04-07 21:16:55 +0000

    Tue, Apr 7, 2026, 5:16 PM -04:00

--- fpdesign.py
+++ fpdesign.py
@@ -3,6 +3,7 @@ print ("Welcome to the CS32 cookbook!\nYou tell us your ingredients, and we will
 ingredients = input("Please list 10 ingredients you have in your kitchen, separated by commas.")
 #Otherwise prompt an error message, "please enter your ingredients separated by commas"
 ingredients.split(",")
+
 #put the ingredients into a list. update the list for each element of the split
 #for each line in ingredients
 #if user input matches ingredients
