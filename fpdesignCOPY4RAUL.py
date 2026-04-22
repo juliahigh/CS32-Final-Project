c823870a98d4b88006a0b1485e128596177ec77d
raulaontiveros <raulaontiveros@gmail.com>
2026-04-22 03:35:00 +0000

    Tue, Apr 21, 2026, 11:35 PM -04:00

---
+++ fpdesignCOPY4RAUL.py
@@ -0,0 +1,13 @@
+print ("Welcome to the CS32 cookbook!\nYou tell us your ingredients, and we will recommend a recipe.\nPress Enter to begin.")
+# require an enter to begin
+ingredients = input("Please list 10 ingredients you have in your kitchen, separated by commas.")
+# Otherwise prompt an error message, "please enter your ingredients separated by commas"
+ingredients.split(",")
+# put the ingredients into a list. update the list for each element of the split
+# for each line in ingredients
+# if user input matches ingredients
+# update some variable
+# percentage of ingredients matched = some variable / total ingredients in recipe
+# print percentage of ingredients matched
+
+
